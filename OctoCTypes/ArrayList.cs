46728092af5724ef55b646747c57d56a4bf639a2
James Fisher <Fisher.James@outlook.com>
2017-01-02 09:58:50 -0800

    Can construct ArrayList with zero or one element.

--- OctoCTypes/ArrayList.cs
+++ OctoCTypes/ArrayList.cs
@@ -7,11 +7,37 @@ namespace OctoCTypes
 {
 	internal class ArrayList<T> : IList<T>
 	{
-		public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		private T[] storage;
+		private int storageSize;
+		private int count;
 
-		public int Count => throw new NotImplementedException();
+		public ArrayList()
+		{
+			storageSize = 0;
+			count = 0;
+		}
 
-		public bool IsReadOnly => throw new NotImplementedException();
+		public ArrayList(T element)
+		{
+			storageSize = 1;
+			storage = new T[1];
+			storage[1] = element;
+			count = 1;
+			
+		}
+
+		public T this[int index] {
+			get {
+				if (index > Count - 1 || index < 0) throw new ArgumentOutOfRangeException("Index was outside the bounds of the ArrayList.");
+				return storage[index];
+			}
+			set {
+				if (index > Count - 1 || index < 0) throw new ArgumentOutOfRangeException("Index was outside the bounds of the ArrayList.");
+				storage[index] = value;
+			} }
+
+		public int Count { get { return count; } }
+		public bool IsReadOnly { get { return false; } }
 
 		public void Add(T item)
 		{
